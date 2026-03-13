8b79c5b748a149df7fce9e97c3e1abb6dee78021
BuffetAntonin <132592381+BuffetAntonin@users.noreply.github.com>
2026-03-13 15:11:40 +0100

    Etape 2 - error pas fini

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,21 @@
 import { Meal } from './meals.js';
+import { ApiError } from './error.js';
 
 export async function fetchMeals(): Promise<Meal[]> {
   try {
     const response = await fetch('https://keligmartin.github.io/api/meals.json');
     
     if (!response.ok) {
-      throw new Error();
+      throw new ApiError("Erreur lors du chargement des repas");
     }
     
     return await response.json();
   } catch (error) {
-    console.error("Erreur lors du chargement des repas");
+    if (error instanceof ApiError) {
+      console.error(error.message);
+    } else {
+      console.error("Erreur lors du chargement des repas");
+    }
     return []; 
   }
 }
